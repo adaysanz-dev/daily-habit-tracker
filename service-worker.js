695be8fceee3e7d2893b2ea0b4b9b7250c024b8e
adaysanz-dev <adaysanz@gmail.com>
2026-07-15 21:00:00 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'daily-habit-tracker-cache-v7';
+const CACHE_NAME = 'daily-habit-tracker-cache-v8';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
